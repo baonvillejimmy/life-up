dd375d5e2533f05176438ff3594f6ac350e5b002
baonvillejimmy <baonville.jimmy@gmail.com>
2019-04-27 15:55:34 +0200

    refactoring mail import

--- life-up/app/Http/Controllers/ContactController.php
+++ life-up/app/Http/Controllers/ContactController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ContactFormRequest;
 use App\Mail\ContactEmail;
-use Mail;
+use Illuminate\Mail;
 
 class ContactController extends Controller
 {
